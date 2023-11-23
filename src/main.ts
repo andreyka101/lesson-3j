017e051563b316359df0240a8c490c4317f4cd37
andreyka101 <andrejkvasnin236@gmail.com>
2023-11-23 21:43:03 +0300

    efeefefefef

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import './style.scss'
+{
+  let canvas = document.querySelector('canvas') as HTMLCanvasElement
+  let ctx = canvas.getContext('2d');
+  ctx?.beginPath()
+  ctx?.moveTo(0, 0);
+  ctx?.lineTo(50,100)
+  ctx?.stroke()
+}
