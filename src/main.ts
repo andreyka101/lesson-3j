c164f9fe680bb0c5ce3d9245d9b8e14c74694a1f
andreyka101 <andrejkvasnin236@gmail.com>
2023-11-23 23:51:19 +0300

    egegegge

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import './style.scss'
 {
   let canvas = document.querySelector('canvas') as HTMLCanvasElement
-  let ctx = canvas.getContext('2d');
-  ctx?.beginPath()
-  ctx?.moveTo(0, 0);
-  ctx?.lineTo(50,100)
-  ctx?.stroke()
+  let ctx = canvas.getContext('2d') as any
+  ctx.beginPath()
+  ctx.moveTo(0, 0);
+  ctx.lineTo(50,100)
+  ctx.lineTo(150,70)
+  ctx.closePath()
+  ctx.fillStyle = '#448a67'
+  ctx.fill()
+  // ctx.stroke()
+  ctx.fillStyle = 'red'
+  ctx.rotate(20 * Math.PI / 2);
+  ctx.fillRect(70,80,50,50)
 }
