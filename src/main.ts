24e6cf2715a056f56021cbc461dde949be465a0b
andreyka101 <andrejkvasnin236@gmail.com>
2023-11-24 12:02:51 +0300

    кпкпкпппккпкп

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
 import './style.scss'
+
 {
   let canvas = document.querySelector('canvas') as HTMLCanvasElement
   let ctx = canvas.getContext('2d') as any
-  ctx.beginPath()
-  ctx.moveTo(0, 0);
-  ctx.lineTo(50,100)
-  ctx.lineTo(150,70)
-  ctx.closePath()
-  ctx.fillStyle = '#448a67'
-  ctx.fill()
   // ctx.stroke()
-  ctx.fillStyle = 'red'
-  ctx.rotate(20 * Math.PI / 2);
-  ctx.fillRect(70,80,50,50)
+  setInterval(() =>{
+    ctx.beginPath()
+    ctx.fillStyle = '#363636'
+    ctx.fillRect(-canvas.width, -canvas.height, canvas.width*canvas.width, canvas.height*canvas.height)
+
+    ctx.moveTo(0, 0);
+    ctx.lineTo(50,100)
+    ctx.lineTo(150,70)
+    ctx.closePath()
+    ctx.fillStyle = '#448a67'
+    ctx.fill()
+    ctx.fillStyle = 'red'
+    ctx.fillRect(70,80,50,50)
+    ctx.fillStyle = '#FFFF00'
+    ctx.fillRect(-100,-80,10,10)
+    ctx.rotate(20 * Math.PI / 360);
+  },200)
 }
