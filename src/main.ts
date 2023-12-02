10c31e8344790e4a448023013b9950934035f1b2
andreyka101 <andrejkvasnin236@gmail.com>
2023-12-02 13:02:41 +0300

    rgrgrg

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,4 @@
 import './style.scss'
 
 {
-  let canvas = document.querySelector('canvas') as HTMLCanvasElement
-  let ctx = canvas.getContext('2d') as any
-  // ctx.stroke()
-  ctx.translate(50,50)
-  setInterval(() =>{
-    ctx.beginPath()
-    ctx.fillStyle = '#363636'
-    ctx.fillRect(-100,-100, canvas.width*canvas.width, canvas.height*canvas.height)
-    
-    // ctx.moveTo(0, 0);
-    // ctx.lineTo(50,100)
-    // ctx.lineTo(150,70)
-    // ctx.closePath()
-    // ctx.fillStyle = '#448a67'
-    // ctx.fill()
-    ctx.fillStyle = 'red'
-    ctx.rotate(1 * Math.PI / 360);
-    ctx.fillRect(-25,-25,50,50)
-  },20)
 }
