128a95208381eb9ab84b23810484ed4c42e87d75
andreyka101 <andrejkvasnin236@gmail.com>
2023-12-03 15:16:05 +0300

    gdggdgd

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,6 @@ import LiveBackgroundMod from './live_background.ts'
 
 let liveBackground = new LiveBackgroundMod()
 liveBackground.start()
+console.log(document.documentElement.clientWidth);
+console.log(document.documentElement.clientHeight);
+
