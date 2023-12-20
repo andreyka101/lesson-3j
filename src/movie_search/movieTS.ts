b0a5e4f0af72ee5e8a33f07e35a570d6b6c682a5
andreyka101 <andrejkvasnin236@gmail.com>
2023-12-20 12:03:27 +0300

    efefefef

--- src/movie_search/movieTS.ts
+++ src/movie_search/movieTS.ts
@@ -24,6 +24,7 @@ const input = document.querySelector("#search input") as HTMLInputElement
 const select = document.querySelector("select") as HTMLSelectElement
 const add = document.querySelector("#add") as HTMLDivElement
 let m_num = 2
+let m_numControl = 10
 let movieALL = "" as any
 
 async function name() {
@@ -97,14 +98,14 @@ async function name() {
             </div>
             <div class="GLASSlOLLIPOPS_div movieBlockFlex" id="m${m_num}" style="">
                 <div class="movieBlock">
-                <img src="${search[i+1].Poster}"
+                <img src="${search[i + 1].Poster}"
                 alt="">
             <div>
                 <span>
-                    ${search[i+1].Title}
+                    ${search[i + 1].Title}
                 </span>
                 <span>
-                ${search[i+1].Year}
+                ${search[i + 1].Year}
                 </span>
                 <span class="longText" style="display: none;">
                     ${PlotFull2}
@@ -112,107 +113,138 @@ async function name() {
                     <span class="shortText">
                     ${PlotFull2.substring(0, 150) + "..."}
                 </span>
-                    </div>
+                </div>
                 </div>
                 <div class="A2buttonBlock" style="display: none;">
-                <a href="https://yandex.ru/search/?text=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i+1].Title}+${search[i+1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&lr=239&clid=2411726" class="GLASSlOLLIPOPS_a" target="_blank">поиск на яндекс.</a>
+                <a href="https://yandex.ru/search/?text=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i + 1].Title}+${search[i + 1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&lr=239&clid=2411726" class="GLASSlOLLIPOPS_a" target="_blank">поиск на яндекс.</a>
                 <button class="GLASSlOLLIPOPS_button collapse">свернуть</button>
-                <a href="https://www.google.com/search?q=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i+1].Title}+${search[i+1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&sca_esv=591540615&sxsrf=AM9HkKnSsl41ffnnpJUkglW8H5wAPZEcPA%3A1702755291217&ei=2_t9Zf3rDKqEwPAPxq6YUA&ved=0ahUKEwi9xZbi2ZSDAxUqAhAIHUYXBgoQ4dUDCBA&oq=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i+1].Title}+${search[i+1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&gs_lp=Egxnd3Mtd2l6LXNlcnAiMtGB0LzQvtGC0YDQtdGC0Ywg0YTQuNC70YzQvCA1NTUg0LHQtdGB0L_Qu9Cw0YLQvdC-SOweUIkGWOsKcAF4AZABAZgB9QOgAY4HqgEHMC4zLjUtMbgBDMgBAPgBAcICChAAGEcY1gQYsAPCAgYQABgIGB7iAwQYACBBiAYBkAYH&sclient=gws-wiz-serp" target="_blank" class="GLASSlOLLIPOPS_a">поиск на google.</a>
+                <a href="https://www.google.com/search?q=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i + 1].Title}+${search[i + 1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&sca_esv=591540615&sxsrf=AM9HkKnSsl41ffnnpJUkglW8H5wAPZEcPA%3A1702755291217&ei=2_t9Zf3rDKqEwPAPxq6YUA&ved=0ahUKEwi9xZbi2ZSDAxUqAhAIHUYXBgoQ4dUDCBA&oq=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i + 1].Title}+${search[i + 1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&gs_lp=Egxnd3Mtd2l6LXNlcnAiMtGB0LzQvtGC0YDQtdGC0Ywg0YTQuNC70YzQvCA1NTUg0LHQtdGB0L_Qu9Cw0YLQvdC-SOweUIkGWOsKcAF4AZABAZgB9QOgAY4HqgEHMC4zLjUtMbgBDMgBAPgBAcICChAAGEcY1gQYsAPCAgYQABgIGB7iAwQYACBBiAYBkAYH&sclient=gws-wiz-serp" target="_blank" class="GLASSlOLLIPOPS_a">поиск на google.</a>
                 </div>
                 <div class="buttonBlock" style="">
                     <button class="GLASSlOLLIPOPS_button expand">развернуть</button>
+                    </div>
+                    </div>
+                    </div>`
+                    m_num += 2
+                }
+
+                
+                response = await fetch(`https://www.omdbapi.com/?&apikey=928973f2&s=${input.value}&page=2&type=${select.value}`)
+                commits = await response.json()
+                console.log(commits.Search)
+                search = commits.Search
+                
+                
+                
+                for (let i = 0; i != 10; i += 2) {
+                    let idFilm = search[i].imdbID
+                    response = await fetch(`https://www.omdbapi.com/?&apikey=928973f2&i=${idFilm}&plot=full`)
+            commits = await response.json()
+            let PlotFull = commits.Plot
+            idFilm = search[i + 1].imdbID
+            response = await fetch(`https://www.omdbapi.com/?&apikey=928973f2&i=${idFilm}&plot=full`)
+            commits = await response.json()
+            let PlotFull2 = commits.Plot
+
+            
+            add.innerHTML += `<div style="display: flex; justify-content: center; flex-wrap: wrap;">
+            <div class="GLASSlOLLIPOPS_div movieBlockFlex" id="m${m_num - 1}" style="">
+                <div class="movieBlock">
+                <img src="${search[i].Poster}"
+                alt="">
+                <div>
+                <span>
+                ${search[i].Title}
+                </span>
+                <span>
+                ${search[i].Year}
+                </span>
+                <span class="longText" style="display: none;">
+                ${PlotFull}
+                </span>
+                <span class="shortText">
+                ${PlotFull.substring(0, 150) + "..."}
+                </span>
                 </div>
-            </div>
-        </div>`
-            m_num += 2
+                </div>
+                <div class="A2buttonBlock" style="display: none;">
+                <a href="https://yandex.ru/search/?text=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i].Title}+${search[i].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&lr=239&clid=2411726" class="GLASSlOLLIPOPS_a" target="_blank">поиск на яндекс.</a>
+                <button class="GLASSlOLLIPOPS_button collapse">свернуть</button>
+                <a href="https://www.google.com/search?q=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i].Title}+${search[i].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&sca_esv=591540615&sxsrf=AM9HkKnSsl41ffnnpJUkglW8H5wAPZEcPA%3A1702755291217&ei=2_t9Zf3rDKqEwPAPxq6YUA&ved=0ahUKEwi9xZbi2ZSDAxUqAhAIHUYXBgoQ4dUDCBA&oq=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i].Title}+${search[i].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&gs_lp=Egxnd3Mtd2l6LXNlcnAiMtGB0LzQvtGC0YDQtdGC0Ywg0YTQuNC70YzQvCA1NTUg0LHQtdGB0L_Qu9Cw0YLQvdC-SOweUIkGWOsKcAF4AZABAZgB9QOgAY4HqgEHMC4zLjUtMbgBDMgBAPgBAcICChAAGEcY1gQYsAPCAgYQABgIGB7iAwQYACBBiAYBkAYH&sclient=gws-wiz-serp" target="_blank" class="GLASSlOLLIPOPS_a">поиск на google.</a>
+                </div>
+                <div class="buttonBlock" style="">
+                    <button class="GLASSlOLLIPOPS_button expand">развернуть</button>
+                    </div>
+                    </div>
+                    <div class="GLASSlOLLIPOPS_div movieBlockFlex" id="m${m_num}" style="">
+                <div class="movieBlock">
+                <img src="${search[i + 1].Poster}"
+                alt="">
+                <div>
+                <span>
+                    ${search[i + 1].Title}
+                    </span>
+                <span>
+                ${search[i + 1].Year}
+                </span>
+                <span class="longText" style="display: none;">
+                ${PlotFull2}
+                </span>
+                <span class="shortText">
+                ${PlotFull2.substring(0, 150) + "..."}
+                </span>
+                </div>
+                </div>
+                <div class="A2buttonBlock" style="display: none;">
+                <a href="https://yandex.ru/search/?text=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i + 1].Title}+${search[i + 1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&lr=239&clid=2411726" class="GLASSlOLLIPOPS_a" target="_blank">поиск на яндекс.</a>
+                <button class="GLASSlOLLIPOPS_button collapse">свернуть</button>
+                <a href="https://www.google.com/search?q=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i + 1].Title}+${search[i + 1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&sca_esv=591540615&sxsrf=AM9HkKnSsl41ffnnpJUkglW8H5wAPZEcPA%3A1702755291217&ei=2_t9Zf3rDKqEwPAPxq6YUA&ved=0ahUKEwi9xZbi2ZSDAxUqAhAIHUYXBgoQ4dUDCBA&oq=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i + 1].Title}+${search[i + 1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&gs_lp=Egxnd3Mtd2l6LXNlcnAiMtGB0LzQvtGC0YDQtdGC0Ywg0YTQuNC70YzQvCA1NTUg0LHQtdGB0L_Qu9Cw0YLQvdC-SOweUIkGWOsKcAF4AZABAZgB9QOgAY4HqgEHMC4zLjUtMbgBDMgBAPgBAcICChAAGEcY1gQYsAPCAgYQABgIGB7iAwQYACBBiAYBkAYH&sclient=gws-wiz-serp" target="_blank" class="GLASSlOLLIPOPS_a">поиск на google.</a>
+                </div>
+                <div class="buttonBlock" style="">
+                    <button class="GLASSlOLLIPOPS_button expand">развернуть</button>
+                    </div>
+                    </div>
+                    </div>`
+                    m_num += 2
+                    console.log(i);
+                }
+
+                
+                
+                let target = document.querySelector('#m'+ m_numControl) as any
+                
+                const body = document.querySelector('body')
+                const callback = (entries: any, observer: any) => {
+    entries.forEach((entry: any) => {
+        if (entry.isIntersecting) {
+            m_numControl +=10
+            target = document.querySelector('#m'+ m_numControl) as any
+            if (target) observer.observe(target)
+            console.log('Элемент пересёк границу области и всё ещё соприкасается с ней!')
+            console.log(target)
+
         }
+    })
+}
+const options = {
+    // root: по умолчанию window, но можно задать любой элемент-контейнер
+    rootMargin: '0px',
+    threshold: 0,
+}
+let observer = new IntersectionObserver(callback, options)
+
+if (target) observer.observe(target)
+console.log(target)
 
 
-        // response = await fetch(`https://www.omdbapi.com/?&apikey=928973f2&s=${input.value}&page=2&type=${select.value}`)
-        // commits = await response.json()
-        // console.log(commits.Search)
-        // search = commits.Search
 
 
-
-        // for (let i = 0; i != 10; i += 2) {
-        //     let idFilm = search[i].imdbID
-        //     response = await fetch(`https://www.omdbapi.com/?&apikey=928973f2&i=${idFilm}&plot=full`)
-        //     commits = await response.json()
-        //     let PlotFull = commits.Plot
-        //     idFilm = search[i + 1].imdbID
-        //     response = await fetch(`https://www.omdbapi.com/?&apikey=928973f2&i=${idFilm}&plot=full`)
-        //     commits = await response.json()
-        //     let PlotFull2 = commits.Plot
-
-
-        //     add.innerHTML += `<div style="display: flex; justify-content: center; flex-wrap: wrap;">
-        //     <div class="GLASSlOLLIPOPS_div movieBlockFlex" id="m${m_num - 1}" style="">
-        //         <div class="movieBlock">
-        //             <img src="${search[i].Poster}"
-        //                 alt="">
-        //             <div>
-        //                 <span>
-        //                     ${search[i].Title}
-        //                 </span>
-        //                 <span>
-        //                     ${search[i].Year}
-        //                 </span>
-        //                 <span class="longText" style="display: none;">
-        //                     ${PlotFull}
-        //                     </span>
-        //                     <span class="shortText">
-        //                     ${PlotFull.substring(0, 150) + "..."}
-        //                 </span>
-        //             </div>
-        //         </div>
-        //         <div class="A2buttonBlock" style="display: none;">
-        //         <a href="https://yandex.ru/search/?text=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i].Title}+${search[i].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&lr=239&clid=2411726" class="GLASSlOLLIPOPS_a" target="_blank">поиск на яндекс.</a>
-        //         <button class="GLASSlOLLIPOPS_button collapse">свернуть</button>
-        //         <a href="https://www.google.com/search?q=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i].Title}+${search[i].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&sca_esv=591540615&sxsrf=AM9HkKnSsl41ffnnpJUkglW8H5wAPZEcPA%3A1702755291217&ei=2_t9Zf3rDKqEwPAPxq6YUA&ved=0ahUKEwi9xZbi2ZSDAxUqAhAIHUYXBgoQ4dUDCBA&oq=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i].Title}+${search[i].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&gs_lp=Egxnd3Mtd2l6LXNlcnAiMtGB0LzQvtGC0YDQtdGC0Ywg0YTQuNC70YzQvCA1NTUg0LHQtdGB0L_Qu9Cw0YLQvdC-SOweUIkGWOsKcAF4AZABAZgB9QOgAY4HqgEHMC4zLjUtMbgBDMgBAPgBAcICChAAGEcY1gQYsAPCAgYQABgIGB7iAwQYACBBiAYBkAYH&sclient=gws-wiz-serp" target="_blank" class="GLASSlOLLIPOPS_a">поиск на google.</a>
-        //         </div>
-        //         <div class="buttonBlock" style="">
-        //             <button class="GLASSlOLLIPOPS_button expand">развернуть</button>
-        //         </div>
-        //     </div>
-        //     <div class="GLASSlOLLIPOPS_div movieBlockFlex" id="m${m_num}" style="">
-        //         <div class="movieBlock">
-        //         <img src="${search[i+1].Poster}"
-        //         alt="">
-        //     <div>
-        //         <span>
-        //             ${search[i+1].Title}
-        //         </span>
-        //         <span>
-        //         ${search[i+1].Year}
-        //         </span>
-        //         <span class="longText" style="display: none;">
-        //             ${PlotFull2}
-        //             </span>
-        //             <span class="shortText">
-        //             ${PlotFull2.substring(0, 150) + "..."}
-        //         </span>
-        //             </div>
-        //         </div>
-        //         <div class="A2buttonBlock" style="display: none;">
-        //         <a href="https://yandex.ru/search/?text=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i+1].Title}+${search[i+1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&lr=239&clid=2411726" class="GLASSlOLLIPOPS_a" target="_blank">поиск на яндекс.</a>
-        //         <button class="GLASSlOLLIPOPS_button collapse">свернуть</button>
-        //         <a href="https://www.google.com/search?q=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i+1].Title}+${search[i+1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&sca_esv=591540615&sxsrf=AM9HkKnSsl41ffnnpJUkglW8H5wAPZEcPA%3A1702755291217&ei=2_t9Zf3rDKqEwPAPxq6YUA&ved=0ahUKEwi9xZbi2ZSDAxUqAhAIHUYXBgoQ4dUDCBA&oq=%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C+%D1%84%D0%B8%D0%BB%D1%8C%D0%BC+${search[i+1].Title}+${search[i+1].Year}+%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE&gs_lp=Egxnd3Mtd2l6LXNlcnAiMtGB0LzQvtGC0YDQtdGC0Ywg0YTQuNC70YzQvCA1NTUg0LHQtdGB0L_Qu9Cw0YLQvdC-SOweUIkGWOsKcAF4AZABAZgB9QOgAY4HqgEHMC4zLjUtMbgBDMgBAPgBAcICChAAGEcY1gQYsAPCAgYQABgIGB7iAwQYACBBiAYBkAYH&sclient=gws-wiz-serp" target="_blank" class="GLASSlOLLIPOPS_a">поиск на google.</a>
-        //         </div>
-        //         <div class="buttonBlock" style="">
-        //             <button class="GLASSlOLLIPOPS_button expand">развернуть</button>
-        //         </div>
-        //     </div>
-        // </div>`
-        //     m_num += 2
-        //     console.log(i);
-        // }
         movieALL = document.querySelectorAll(".movieBlockFlex") as any
         buttonMovie()
 
 
 
-        
+
         // let response = await fetch("https://www.omdbapi.com/?&apikey=928973f2&s=max")
         // let commits = await response.json()
 
@@ -423,3 +455,4 @@ function buttonMovie() {
         })
     }
 }
+
